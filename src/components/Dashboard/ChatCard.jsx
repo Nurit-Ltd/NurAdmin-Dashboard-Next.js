68d342766aa0e5fffbd34b355d4b899825a38a16
SymuNadeem <msymunnadeem@gmail.com>
2024-04-04 23:48:33 +0600

    home page components

---
+++ src/components/Dashboard/ChatCard.jsx
@@ -0,0 +1,153 @@
+import Image from "next/image";
+import Link from "next/link";
+import user6 from "../../assets/images/team-01.png";
+import user1 from "../../assets/images/user-03.png";
+import user2 from "../../assets/images/user-22.png";
+import user3 from "../../assets/images/user-23.png";
+import user4 from "../../assets/images/user-24.png";
+import user5 from "../../assets/images/user-25.png";
+
+const ChatCard = () => {
+  return (
+    <div className="col-span-12 rounded-sm border border-stroke bg-white py-6 shadow-md xl:col-span-4">
+      <h4 className="mb-6 px-7 text-xl font-bold text-black">Chats</h4>
+      <div>
+        <Link
+          href={"/message"}
+          className="flex items-center gap-5 px-7 py-3 hover:bg-gray-100"
+        >
+          <div className="relative h-14 w-14 rounded-full">
+            <Image src={user1} alt="User" />
+            <span className="absolute bottom-0 right-0 h-3.5 w-3.5 rounded-full border-2 border-white bg-green-500"></span>
+          </div>
+
+          <div className="flex flex-1 items-center justify-between">
+            <div>
+              <h5 className="font-medium text-black">Devid Heilo</h5>
+              <p>
+                <span className="text-sm font-medium text-black">
+                  Hello, how are you?
+                </span>
+                <span className="text-xs text-secondary"> . 12 min</span>
+              </p>
+            </div>
+            <div className="flex h-6 w-6 items-center justify-center rounded-full bg-primary">
+              <span className="text-sm font-medium text-white">3</span>
+            </div>
+          </div>
+        </Link>
+        <Link
+          href={"/message"}
+          className="flex items-center gap-5 px-7 py-3 hover:bg-gray-100"
+        >
+          <div className="relative h-14 w-14 rounded-full">
+            <Image src={user2} alt="User" />
+            <span className="absolute bottom-0 right-0 h-3.5 w-3.5 rounded-full border-2 border-white bg-green-500"></span>
+          </div>
+
+          <div className="flex flex-1 items-center justify-between">
+            <div>
+              <h5 className="font-medium text-secondary">Henry Fisher</h5>
+              <p>
+                <span className="text-sm font-medium text-secondary">
+                  I am waiting for you
+                </span>
+                <span className="text-xs text-secondary"> . 5:54 PM</span>
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link
+          href={"/message"}
+          className="flex items-center gap-5 px-7 py-3 hover:bg-gray-100"
+        >
+          <div className="relative h-14 w-14 rounded-full">
+            <Image src={user3} alt="User" />
+            <span className="absolute bottom-0 right-0 h-3.5 w-3.5 rounded-full border-2 border-white bg-orange-400"></span>
+          </div>
+
+          <div className="flex flex-1 items-center justify-between">
+            <div>
+              <h5 className="font-medium text-secondary">Wilium Smith</h5>
+              <p>
+                <span className="text-sm font-medium text-secondary">
+                  Where are you now?
+                </span>
+                <span className="text-xs text-secondary"> . 10:12 PM</span>
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link
+          href={"/message"}
+          className="flex items-center gap-5 px-7 py-3 hover:bg-gray-100"
+        >
+          <div className="relative h-14 w-14 rounded-full">
+            <Image src={user4} alt="User" />
+            <span className="absolute bottom-0 right-0 h-3.5 w-3.5 rounded-full border-2 border-white bg-green-500"></span>
+          </div>
+
+          <div className="flex flex-1 items-center justify-between">
+            <div>
+              <h5 className="font-medium text-black ">Henry Deco</h5>
+              <p>
+                <span className="text-sm font-medium text-black ">
+                  Thank you so much!
+                </span>
+                <span className="text-xs text-secondary"> . Sun</span>
+              </p>
+            </div>
+            <div className="flex h-6 w-6 items-center justify-center rounded-full bg-primary">
+              <span className="text-sm font-medium text-white">2</span>
+            </div>
+          </div>
+        </Link>
+        <Link
+          href={"/message"}
+          className="flex items-center gap-5 px-7 py-3 hover:bg-gray-100"
+        >
+          <div className="relative h-14 w-14 rounded-full">
+            <Image src={user5} alt="User" />
+            <span className="absolute bottom-0 right-0 h-3.5 w-3.5 rounded-full border-2 border-white bg-orange-600"></span>
+          </div>
+
+          <div className="flex flex-1 items-center justify-between">
+            <div>
+              <h5 className="font-medium text-secondary">Jubin Jack</h5>
+              <p>
+                <span className="text-sm font-medium text-secondary">
+                  I really love that!
+                </span>
+                <span className="text-xs text-secondary"> . Oct 23</span>
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link
+          href={"/message"}
+          className="flex items-center gap-5 px-7 py-3 hover:bg-gray-100"
+        >
+          <div className="relative h-14 w-14 rounded-full">
+            <Image src={user6} alt="User" />
+
+            <span className="absolute bottom-0 right-0 h-3.5 w-3.5 rounded-full border-2 border-white bg-yellow-600"></span>
+          </div>
+
+          <div className="flex flex-1 items-center justify-between">
+            <div>
+              <h5 className="font-medium text-secondary">Wilium Smith</h5>
+              <p>
+                <span className="text-sm font-medium text-secondary">
+                  Where are you now?
+                </span>
+                <span className="text-xs text-secondary"> . Sep 20</span>
+              </p>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ChatCard;
---
+++ src/components/Dashboard/TopChannels.jsx
@@ -0,0 +1,111 @@
+import source1 from '../../assets/images/brand-01.svg'
+import source2 from '../../assets/images/brand-02.svg'
+import source3 from '../../assets/images/brand-03.svg'
+import source4 from '../../assets/images/brand-04.svg'
+import source5 from '../../assets/images/brand-05.svg'
+const TopChannels = () => {
+    const channels = [
+        {
+            Source: 'Google',
+            Image: source1,
+            Visitors: '3.5K',
+            Revenues: '$5,768',
+            Sales: '590',
+            Conversion: '4.8%',
+        },
+        {
+            Source: 'Twitter',
+            Image: source2,
+            Visitors: '2.3K',
+            Revenues: '$3,456',
+            Sales: '480',
+            Conversion: '3.2%',
+        },
+        {
+            Source: 'Github',
+            Image: source3,
+            Visitors: '1.8K',
+            Revenues: '$2,987',
+            Sales: '390',
+            Conversion: '3.9%',
+        },
+        {
+            Source: 'Vimeo',
+            Image: source4,
+            Visitors: '4.2K',
+            Revenues: '$7,234',
+            Sales: '680',
+            Conversion: '5.1%',
+        },
+        {
+            Source: 'Facebook',
+            Image: source5,
+            Visitors: '2.8K',
+            Revenues: '$4,890',
+            Sales: '550',
+            Conversion: '4.3%',
+        }
+    ];
+
+
+    return (
+        <div className='rounded-sm border border-stroke bg-white px-5 pb-2 pt-6 shadow-md sm:px-7 xl:pb-1'>
+            <h4 className="mb-6 text-xl font-bold text-black">
+                Top Channels
+            </h4>
+            <div className="flex flex-col">
+                <div className="grid grid-cols-3 rounded-sm bg-gray-100 md:grid-cols-5">
+                    <div className="p-2 xl:p-5">
+                        <h5 className="text-sm font-medium uppercase lg:text-base text-secondary">Source</h5>
+                    </div>
+                    <div className="p-2 text-center xl:p-5">
+                        <h5 className="text-sm font-medium uppercase lg:text-base text-secondary">Visitors</h5>
+                    </div>
+                    <div className="p-2 text-center xl:p-5">
+                        <h5 className="text-sm font-medium uppercase lg:text-base text-secondary">Revenues</h5>
+                    </div>
+                    <div className="hidden p-2 text-center sm:block xl:p-5">
+                        <h5 className="text-sm font-medium uppercase md:text-base text-secondary">Sales</h5>
+                    </div>
+                    <div className="hidden p-2 text-center sm:block xl:p-5">
+                        <h5 className="text-sm font-medium uppercase md:text-base text-secondary">Conversion</h5>
+                    </div>
+                </div>
+                {
+                    channels.map((channel, idx) => (
+                        <div key={idx} className={`grid grid-cols-3 ${idx !== channels.length - 1 ? "border-b" : ""}  border-stroke md:grid-cols-5`}>
+                            <div className="flex items-center gap-3 p-2 xl:p-5">
+                                <div className="flex-shrink-0">
+                                    <img src={channel.Image} alt="Brand" />
+                                </div>
+                                <p className="hidden font-medium text-black sm:block">
+                                    {channel.Source}
+                                </p>
+                            </div>
+
+                            <div className="flex items-center justify-center p-2 xl:p-5">
+                                <p className="font-medium text-black ">{channel.Visitors}</p>
+                            </div>
+
+                            <div className="flex items-center justify-center p-2 xl:p-5">
+                                <p className="font-medium text-green-500">{channel.Revenues}</p>
+                            </div>
+
+                            <div className="hidden items-center justify-center p-2 sm:flex xl:p-5">
+                                <p className="font-medium text-black">{channel.Sales}</p>
+                            </div>
+
+                            <div className="hidden items-center justify-center p-2 sm:flex xl:p-5">
+                                <p className="font-medium text-blue-400">{channel.Conversion}</p>
+                            </div>
+                        </div>
+                    ))
+                }
+
+            </div>
+        </div>
+    )
+}
+
+export default TopChannels
+
